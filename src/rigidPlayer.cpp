9c53571f08d3364097888044a198f3fb7021c621
HoneyHiccups <OrionThompson@protonmail.com>
2026-01-29 04:35:13 -0800

    yes

---
+++ src/rigidPlayer.cpp
@@ -0,0 +1,15 @@
+#include "rigidPlayer.hpp"
+
+
+void RigidPlayer::_bind_method(){
+
+}
+
+
+void RigidPlayer::_ready(){
+
+
+    _virtual_ready();
+}
+
+
---
+++ src/rigidPlayer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "godot_cpp/classes/node3d.hpp"
+#include "godot_cpp/classes/rigid_body3d.hpp"
+#include "godot_cpp/classes/wrapped.hpp"
+
+class RigidPlayer : public godot::RigidBody3D{
+    GDCLASS(RigidPlayer, godot::RigidBody3D)
+
+    protected:
+    static void _bind_method();
+    void _ready() override;
+
+    private:
+        void virtual _virtual_ready();
+
+    public:
+
+
+
+    protected:
+
+    private:
+        godot::Node3D piv_body;
+        godot::Node3D piv_head;
+        godot::Node3D piv_camrea_shake;
+
+    public:
+    
+};
